b51894ad2ea84fb26e29c711d8bdcf938531cac9
MD-ALAMIN-HOSSEN <mdalaminhossen223322@gmail.com>
2024-07-10 17:48:45 +0600

    all database entity are created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,51 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Login } from './entity/Login.entity';
+import { Info } from './entity/Info.entity';
+import { Admin } from './entity/Admin.entity';
+import { Manager } from './entity/Manager.entity';
+import { Customer } from './entity/Customer.entity';
+import { Agent } from './entity/Agent.entity';
+import { CustomerAgent } from './entity/CustomerAgent.entity';
+import { AdminManager } from './entity/AdminManager.entity';
+import { ManagerAgent } from './entity/ManagerAgent.entity';
+import { Bus } from './entity/Bus.entity';
+import { Ticket } from './entity/Ticket.entity';
+import { Feedback } from './entity/Feedback.entity';
+import { Report } from './entity/Report.entity';
+import { Notification } from './entity/Notification.entity';
+import { TicketBookInfo } from './entity/TicketBookInfo.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(
+    { type: 'postgres',
+    host: 'localhost',
+    port: 5001,
+    username: 'postgres',
+    password: '123',
+    database: 'book ticket db',//Change to your database name
+    entities: [
+      Login,
+      Info,
+      Admin,
+      Manager,
+      Customer,
+      Agent,
+      CustomerAgent,
+      AdminManager,
+      ManagerAgent,
+      Bus,
+      Ticket,
+      Feedback,
+      Report,
+      Notification,
+      TicketBookInfo,
+    ],
+    autoLoadEntities: true,
+    synchronize: true,
+    } )],
   controllers: [AppController],
   providers: [AppService],
 })
